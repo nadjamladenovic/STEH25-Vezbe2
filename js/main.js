74fe8919479e3b52ac2cd3d2d660cd16afdbf2a9
nadjamladenovic <nadjamladenovic26@gmail.com>
2025-07-03 11:35:52 +0200

    Implementirano asinhrono dodavanje

--- js/main.js
+++ js/main.js
@@ -1,24 +1,80 @@
 console.log("Ucitan JavaScript");
 
+console.log("Povezan JavaScript fajl")
+
+$('#dodajForm').submit(function(event){
+    event.preventDefault();
+    console.log("dodavanje je pokrenuto")
+
+    //pokupiti podatke sa forme
+    const $form = $(this);
+    console.log($form);
+
+    const serialForm = $form.serializeArray();
+    console.log(serialForm);
+
+    const newRow = serialForm.reduce(
+        (json,{name,value}) => ( (json[name] = value), json)
+    )
+
+    $.ajax(
+        {
+            url: "obrada.php",
+            type: "post",
+            data: serialForm,
+        }
+    ).done(
+        function(response){
+            console.log(response);
+            if(!isNaN(response)){
+                console.log("Prijava dodata.");
+                appendRow({
+                    id:response,
+                    ...newRow
+                })
+            }else{
+                console.log("Prijava NIJE dodata");
+            }
+        }
+    ).fail(
+        function(errorThrown){
+            console.error('Greska prilikom dodavanja prijave')
+        }
+    )
+});
+
+function appendRow(data){
+    $('#myTable tbody').append(
+        `
+        <tr>
+            <td>${data.predmet}</td>
+            <td>${data.katedra}</td>
+            <td>${data.sala}</td>
+            <td>${data.datum}</td>
+            <td>
+                <label class="custom-radio-btn">
+                    <input type="radio" name="id_predmeta" value="${data.id}">
+                    <span class="checkmark"></span>
+                </label>
+            </td>
+        </tr>
+        `
+    )
+}
 $('#btn-obrisi').click(function(event){
     event.preventDefault();
     console.log("Usao u brisanje")
-
-    const field = $("input[name=id_predmeta]:checked");
-
-    $.ajax({
-        url: "odbrana.php",
-        type: "post",
-        data: {
-            "id_predmeta": field.val()
+            url: "obrada.php";
+            type: "post";
+            data: {
+                "id_necega"; field.val()
+            }
         }
-    }
     ).done(function(response){
         console.log(response)
         if(response === "uspesno obrisano"){
             field.closest("tr").remove();
-        }else{
+        } else {
             console.log("neuspesno");
         }
     })
-})
